aa30cc4e96d74748f7330da4e00b6c2f80c68103
nelu <wf.nelu@yandex.ru>
2019-10-11 12:20:02 +0200

    Modification fonction jouer()

--- Bataille/src/Joueur.java
+++ Bataille/src/Joueur.java
@@ -43,51 +43,119 @@ public class Joueur {
     }
 
     public void jouer(Joueur adversaire) {
+        ArrayList<Carte> paquetPartie = new ArrayList<Carte>();
         int i = this.getPaquet().size();
         int j = adversaire.getPaquet().size();
+
         System.out.println("\n" + i + " " + j);
 
         while (i!=0 && j!=0) {
-            String resultat = this.getPaquet().get(0).comparateur(adversaire.getPaquet().get(0));
-            if (resultat == "Votre carte est moins forte que la carte de l'adversaire") {
-                System.out.println("\n" + resultat);
-                adversaire.getPaquet().add(this.getPaquet().get(0));
-                this.getPaquet().remove(this.getPaquet().get(0));
-            } else if (resultat == "Votre carte est plus forte que la carte de l'adversaire") {
-                System.out.println("\n" + resultat);
-                this.getPaquet().add(adversaire.getPaquet().get(0));
-                adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+            paquetPartie.add(this.getPaquet().get(0));
+            paquetPartie.add(adversaire.getPaquet().get(0));
+
+            this.getPaquet().remove(this.getPaquet().get(0));
+            adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+
+            String resultat1 = paquetPartie.get(0).comparateur(paquetPartie.get(1));
+            if(resultat1 == "Votre carte est moins forte que la carte de l'adversaire") {
+                adversaire.getPaquet().add(paquetPartie.get(0));
+                adversaire.getPaquet().add(paquetPartie.get(1));
+                paquetPartie.clear();
+            } else if(resultat1 == "Votre carte est plus forte que la carte de l'adversaire") {
+                this.getPaquet().add(paquetPartie.get(1));
+                this.getPaquet().add(paquetPartie.get(0));
+                paquetPartie.clear();
             } else {
-                System.out.println("\n" + resultat);
+                paquetPartie.add(this.getPaquet().get(0));
+                this.getPaquet().remove(this.getPaquet().get(0));
+                paquetPartie.add(adversaire.getPaquet().get(0));
+                adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
 
-                String message = this.getPaquet().get(1).comparateur(adversaire.getPaquet().get(1));
-                if (message == "Votre carte est moins forte que la carte de l'adversaire" ) {
-                    adversaire.getPaquet().add(this.getPaquet().get(1));
-                    adversaire.getPaquet().add(this.getPaquet().get(0));
+                paquetPartie.add(this.getPaquet().get(0));
+                this.getPaquet().remove(this.getPaquet().get(0));
+                paquetPartie.add(adversaire.getPaquet().get(0));
+                adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
 
+                String resultat2 = paquetPartie.get(4).comparateur(paquetPartie.get(5));
+                if(resultat2 == "Votre carte est moins forte que la carte de l'adversaire"){
+                    for(int n=0; n<6; n++) {
+                        adversaire.getPaquet().add(paquetPartie.get(n));
+                    }
+                    paquetPartie.clear();
+                } else if(resultat2 == "Votre carte est plus forte que la carte de l'adversaire") {
+                    for(int m=0; m<6; m++) {
+                        this.getPaquet().add(paquetPartie.get(m));
+                    }
+                    paquetPartie.clear();
+                } else {
+                    paquetPartie.add(this.getPaquet().get(0));
                     this.getPaquet().remove(this.getPaquet().get(0));
+                    paquetPartie.add(adversaire.getPaquet().get(0));
+                    adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+
+                    paquetPartie.add(this.getPaquet().get(0));
                     this.getPaquet().remove(this.getPaquet().get(0));
-
-                    adversaire.getPaquet().add(adversaire.getPaquet().get(1));
-                    adversaire.getPaquet().add(adversaire.getPaquet().get(0));
-
-                    adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
-                    adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
-                    Collections.shuffle(adversaire.getPaquet());
-
-                } else if (message == "Votre carte est plus forte que la carte de l'adversaire") {
-                    this.getPaquet().add(adversaire.getPaquet().get(1));
-                    this.getPaquet().add(adversaire.getPaquet().get(0));
-
-                    adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+                    paquetPartie.add(adversaire.getPaquet().get(0));
                     adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
 
-                    this.getPaquet().add(this.getPaquet().get(1));
-                    this.getPaquet().add(this.getPaquet().get(0));
+                    String resultat3 = paquetPartie.get(8).comparateur(paquetPartie.get(9));
+                    if(resultat3 == "Votre carte est moins forte que la carte de l'adversaire"){
+                        for(int k=0; k<10; k++) {
+                            adversaire.getPaquet().add(paquetPartie.get(k));
+                        }
+                        paquetPartie.clear();
+                    } else if(resultat3 == "Votre carte est plus forte que la carte de l'adversaire") {
+                        for(int l=0; l<10; l++) {
+                            this.getPaquet().add(paquetPartie.get(l));
+                        }
+                        paquetPartie.clear();
+                    } else {
+                        paquetPartie.add(this.getPaquet().get(0));
+                        this.getPaquet().remove(this.getPaquet().get(0));
+                        paquetPartie.add(adversaire.getPaquet().get(0));
+                        adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
 
-                    this.getPaquet().remove(this.getPaquet().get(0));
-                    this.getPaquet().remove(this.getPaquet().get(0));
-                    Collections.shuffle(this.getPaquet());
+                        paquetPartie.add(this.getPaquet().get(0));
+                        this.getPaquet().remove(this.getPaquet().get(0));
+                        paquetPartie.add(adversaire.getPaquet().get(0));
+                        adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+
+                        String resultat4 = paquetPartie.get(12).comparateur(paquetPartie.get(13));
+                        if(resultat4 == "Votre carte est moins forte que la carte de l'adversaire"){
+                            for(int c=0; c<14; c++) {
+                                adversaire.getPaquet().add(paquetPartie.get(c));
+                            }
+                            paquetPartie.clear();
+                        } else if(resultat4 == "Votre carte est plus forte que la carte de l'adversaire") {
+                            for(int v=0; v<14; v++) {
+                                this.getPaquet().add(paquetPartie.get(v));
+                            }
+                            paquetPartie.clear();
+                        } else {
+                            paquetPartie.add(this.getPaquet().get(0));
+                            this.getPaquet().remove(this.getPaquet().get(0));
+                            paquetPartie.add(adversaire.getPaquet().get(0));
+                            adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+
+                            paquetPartie.add(this.getPaquet().get(0));
+                            this.getPaquet().remove(this.getPaquet().get(0));
+                            paquetPartie.add(adversaire.getPaquet().get(0));
+                            adversaire.getPaquet().remove(adversaire.getPaquet().get(0));
+
+                            String resultat5 = paquetPartie.get(16).comparateur(paquetPartie.get(17));
+                            if(resultat5 == "Votre carte est moins forte que la carte de l'adversaire"){
+                                for(int w=0; w<18; w++) {
+                                    adversaire.getPaquet().add(paquetPartie.get(w));
+                                }
+                                paquetPartie.clear();
+                            } else if(resultat5 == "Votre carte est plus forte que la carte de l'adversaire") {
+                                for(int x=0; x<18; x++) {
+                                    this.getPaquet().add(paquetPartie.get(x));
+                                }
+                                paquetPartie.clear();
+                            }
+                        }
+                    }
                 }
             }
 
