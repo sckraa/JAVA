f043aaa342a3a2ebb36cd924c313384e44828dbc
nelu <wf.nelu@yandex.ru>
2019-10-22 23:46:13 +0200

    Correction bug push : Class FichierTexte

--- LectureFichier/src/FichierTexte.java
+++ LectureFichier/src/FichierTexte.java
@@ -1,50 +1,30 @@
-import java.io.*;
+import java.io.File;
 import java.util.ArrayList;
 
-public class LectureFichier {
-    private File fichier;
-    private ArrayList<String> contenuFichier;
+public class FichierTexte extends  LectureFichier implements LectureFichierTexte{
 
 // Constructeurs
-    public LectureFichier() {
+    public FichierTexte() {
         this.contenuFichier = new ArrayList<>();
     }
-    public LectureFichier(String nomFichier) {
+    public FichierTexte(String nomFichier) {
         this.fichier = new File(nomFichier);
         this.contenuFichier = new ArrayList<>();
     }
 
-// Getters et Seters
-    public File getFichier() { return this.fichier; }
-    public void setFichier(File fichier) { this.fichier = fichier; }
-
-    public ArrayList<String> getContenuFichier() { return contenuFichier; }
-    public void setContenuFichier(ArrayList<String> contenuFichier) { this.contenuFichier = contenuFichier; }
-
 // Méthodes
-    public void lireFichier() {
-        boolean fichierExiste = this.getFichier().exists();
-        File fichier = this.getFichier();
-        if (fichierExiste) {
-            try {
-                FileInputStream fichierFlux = new FileInputStream(fichier);
-                InputStreamReader lectureFichier = new InputStreamReader(fichierFlux);
-                BufferedReader memoire = new BufferedReader(lectureFichier);
-                String ligneFichier = "";
-                while ((ligneFichier=memoire.readLine()) != null) {
-                    this.getContenuFichier().add(ligneFichier);
-                }
-                memoire.close();
-            } catch (Exception e) {
-                System.out.println(e.toString());
-            }
-        } else {
-            System.out.println("Le fichier ["+ this.getFichier() +"] n'existe pas.");
+    public void lectureInversee() {
+        for(int i=this.getContenuFichier().size()-1; i>=0; i--) {
+            System.out.println(this.getContenuFichier().get(i));
         }
     }
-    public void afficherContenu() {
-        int tailleContenu = this.getContenuFichier().size();
-        for(int i=0; i<tailleContenu; i++) {
+    public void lecturePalindromique() {
+        for(int i=this.getContenuFichier().size()-1; i>=0; i--) {
+            for(int j=this.getContenuFichier().get(i).length()-1; j>=0; j--) {
+                this.getContenuFichier().add(Character.toString(this.getContenuFichier().get(i).charAt(j)));
+            }
+        }
+        for(int i=0; i<this.getContenuFichier().size(); i++) {
             System.out.println(this.getContenuFichier().get(i));
         }
     }
