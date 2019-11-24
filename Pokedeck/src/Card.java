f84a454ea91a44106535d5dbd84a4f2bd2e117b5
nelu <wf.nelu@yandex.ru>
2019-11-24 13:22:14 +0100

    Creating a card

---
+++ Pokedeck/src/Card.java
@@ -0,0 +1,46 @@
+public class Card {
+    private Integer id;
+    private String name;
+    private String type;
+    private String description;
+
+    // Builders
+    public Card() {
+        this.id = null;
+        this.name = "undefined";
+        this.type = "undefined";
+        this.description = "undefined";
+    }
+
+    public Card( Integer cardId, String cardName, String cardType, String cardDescription ) {
+        this.id = cardId;
+        this.name = cardName;
+        this.type = cardType;
+        this.description = cardDescription;
+    }
+
+    // Getters
+    public Integer getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getType() {
+        return this.type;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+
+    // Setters
+    public void setDescription ( String description ) {
+        this.description = description;
+    }
+
+    // toString()
+    public String toString() {
+        String infoCard = "[ Card ID: " + this.id + ", Card Name : " + this.name + ", Card Type : " + this.type + ", Card Description : " + this.description + " ]";
+        return infoCard;
+    }
+}
--- Pokedeck/src/Pokedeck.java
+++ Pokedeck/src/Pokedeck.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class Pokedeck {
     public static void main(String[] args) {
-        System.out.println("POKEDECK v1-Terminal");
+        //System.out.println("POKEDECK v1-Terminal");
 
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
 
-        MainPanel menu = new MainPanel(scanner);
+        //MainPanel menu = new MainPanel(scanner);
 
-        menu.start();
+        //menu.start();
+
+        Card pika = new Card(1,"pika", "electro", "powerful card");
+        System.out.println( pika.toString() );
     }
 }
