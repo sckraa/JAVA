4ab73e9472a530213bb9643ef2849b5100787770
nelu <wf.nelu@yandex.ru>
2019-11-24 16:28:07 +0100

    Add deck

---
+++ Pokedeck/src/Deck.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> deck;
+
+    // Builders
+    public Deck() {
+        this.deck = new ArrayList<>();
+    }
+
+    // Methods
+    public void addCard( Card card ) {
+        this.deck.add( card );
+    }
+
+    public boolean deleteCard( Integer id ) {
+        for ( Card card : deck ) {
+            if ( card.getId() == id ) {
+                System.out.println( "Card successfully deleted" );
+                return deck.remove(card);
+            }
+        }
+        return false;
+    }
+
+    public void showDeck() {
+        for ( Card card : deck ) {
+            System.out.println( card.toString() );
+        }
+    }
+}
--- Pokedeck/src/Pokedeck.java
+++ Pokedeck/src/Pokedeck.java
@@ -11,6 +11,25 @@ public class Pokedeck {
         //menu.start();
 
         Card pika = new Card(1,"pika", "electro", "powerful card");
+        Card toto = new Card(2,"toto", "feu", "powerful card");
+        Card titi = new Card(3,"titi", "vent", "powerful card");
+/*
         System.out.println( pika.toString() );
+        System.out.println( toto.toString() );
+        System.out.println( titi.toString() );
+*/
+        System.out.println("----- ------ -----");
+
+        Deck pokedeck = new Deck();
+        pokedeck.addCard(pika);
+        pokedeck.addCard(toto);
+        pokedeck.addCard(titi);
+
+        pokedeck.showDeck();
+
+        // Delete card
+        pokedeck.deleteCard(1);
+        pokedeck.showDeck();
+
     }
 }
