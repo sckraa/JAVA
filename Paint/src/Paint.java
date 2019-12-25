1b38eb8427b9096c450daf779ce71832f6e29cc4
nelu <wf.nelu@yandex.ru>
2019-12-25 22:34:05 +0100

    Paint v1.0

--- Paint/src/Paint.java
+++ Paint/src/Paint.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -10,7 +13,6 @@ public class Paint extends JFrame {
 
     // SECTIONS
     private JPanel west;
-    private Color colorWestPanel;
     private PaintArea paintArea;
 
     // MENU
@@ -26,33 +28,46 @@ public class Paint extends JFrame {
 
     // MENU ITEMS
     private JMenuItem save;
-    private JMenuItem saveAs;
     private JMenuItem exit;
 
     // TOOL SECTION
     private Color color;
-    private JSlider sliderPenSize;
     private JLabel colorLabel;
-    private JButton colorPick;
     private JPanel colorPanel;
-    private JButton gum;
+    private JButton colorPick;
+
+    private JLabel colors;
+    private JButton red;
+    private JButton green;
+    private JButton blue;
+    private JButton black;
+
+    private JSlider sliderPenSize;
+
+    private JButton eraser;
+    private JButton reset;
+    private JButton back;
 
     public Paint() {
-        final int width = 1500;
-        final int height = 1000;
+        final int width = 900;
+        final int height = 700;
 
-        setTitle("Paint");
-        setSize(width, height);
-        setLocation(300, 10);
+        setTitle( "Paint" );
+        setSize( width, height );
+        setLocation( 300, 10 );
+        setIconImage( Toolkit.getDefaultToolkit().getImage(getClass().getResource( "./assets/img/icon.png" ) ) );
+        getRootPane().setBorder( BorderFactory.createEmptyBorder( 0, 5, 5, 5 ) );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
+        // MAIN CONTAINER
         Container mainContainer = this.getContentPane();
-        mainContainer.setLayout(new BorderLayout(5, 5));
-        this.getRootPane().setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("./assets/img/icon.png")));
+        mainContainer.setBackground( new Color(204, 204, 204) );
+        mainContainer.setLayout( new BorderLayout( 5, 5 ) );
+
+        // INIT PAINT
         init();
 
-        setVisible(true);
+        setVisible( true );
     }
 
     private void init() {
@@ -61,104 +76,251 @@ public class Paint extends JFrame {
     }
 
     public void Menu() {
-        this.menu = new JMenuBar();
+        // MENU BAR
+        menu = new JMenuBar();
 
-        this.file = new JMenu("File");
-        this.edit = new JMenu("Edit");
-        this.view = new JMenu("View");
-        this.selection = new JMenu("Selection");
-        this.find = new JMenu("Find");
-        this.about = new JMenu("About");
-        this.help = new JMenu("Help");
+        // MENU
+        file = new JMenu( "File" );
+        edit = new JMenu( "Edit" );
+        view = new JMenu( "View" );
+        selection = new JMenu( "Selection" );
+        find = new JMenu( "Find" );
+        about = new JMenu( "About" );
+        help = new JMenu( "Help" );
 
-        this.save = new JMenuItem("Save");
-        this.saveAs = new JMenuItem("Save As...");
-        this.exit = new JMenuItem("Exit");
+        // MENU ITEM
+        save = new JMenuItem( "Save" );
+        exit = new JMenuItem( "Exit" );
 
-        this.colorMenu = new Color(204, 204, 204);
+        colorMenu = new Color( 204, 204, 204 );
 
-        this.menu.add(this.file);
-        this.menu.add(this.edit);
-        this.menu.add(this.view);
-        this.menu.add(this.selection);
-        this.menu.add(this.find);
-        this.menu.add(this.about);
-        this.menu.add(this.help);
-        this.file.add(this.save);
-        this.file.add(this.saveAs);
-        this.file.add(this.exit);
-        setJMenuBar(this.menu);
-        this.menu.setBackground(this.colorMenu);
+        // SAVE DRAW
+        save.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent actionEvent ) {
+                BufferedImage image = new BufferedImage( paintArea.getWidth(), paintArea.getHeight(), BufferedImage.TYPE_INT_RGB );
+                paintArea.paint( image.getGraphics() );
+                try{
+                    ImageIO.write( image, "png", new File( "./src/assets/draw.png" ) );
+                } catch ( Exception e ) {
+                    System.out.println( "Draw not saved " + e.getMessage() );
+                }
+            }
+        });
+
+        // EXIT APPLICATION
+        exit.setMnemonic( KeyEvent.VK_Q );
+        exit.setAccelerator( KeyStroke.getKeyStroke( KeyEvent.VK_Q, InputEvent.CTRL_MASK ) );
+        exit.setToolTipText( "Exit application" );
+        exit.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent event ) {
+                System.exit( 0 );
+            }
+        });
+
+        // ADD MENU
+        menu.add( file );
+        menu.add( edit );
+        menu.add( view );
+        menu.add( selection );
+        menu.add( find );
+        menu.add( about );
+        menu.add( help );
+        file.add( save );
+        file.add( exit );
+        setJMenuBar( menu );
+        menu.setBackground( colorMenu );
     }
 
     public void PanelWest() {
-        this.west = new JPanel();
-        this.west.setLayout(new FlowLayout());
+        west = new JPanel();
+        west.setLayout( new FlowLayout() );
 
         JPanel gridPanel = new JPanel();
-        gridPanel.setLayout(new GridLayout(10, 1, 1, 10));
+        gridPanel.setLayout( new GridLayout( 14, 1, 1, 10 ) );
+        gridPanel.setBorder( BorderFactory.createEmptyBorder( 20, 10, 20, 10 ) );
 
-        // COLOR choice
-        this.color = Color.RED;
-        this.colorLabel = new JLabel("Color : ");
-        this.colorPick = new JButton("Choose a color");
-        this.colorPanel = new JPanel();
-        this.colorPanel.setBackground(color);
 
-        colorPick.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                color = JColorChooser.showDialog(null, "Pick your color", color);
-                paintArea.setPenColor(color);
-                colorPanel.setBackground(color);
+        // COLOR CHOICE
+        color = Color.BLACK;
+        colorLabel = new JLabel( "Color : " );
+        colorPick = new JButton( "Choose a color" );
+        colorPanel = new JPanel();
+        colorPanel.setBackground( color );
+
+        // ACTION LISTENER FOR COLOR PICK BUTTON
+        colorPick.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                color = JColorChooser.showDialog( null, "Pick your color", color );
+
+                if ( color == null ) {
+                    color = Color.BLACK;
+                }
+
+                paintArea.setPenColor( color );
+                colorPanel.setBackground( color );
             }
         });
+
+
+        // COLORS RGB
+        colors = new JLabel( "Colors : " );
+        black = new JButton( "BLACK" );
+        black.setBackground( Color.BLACK );
+        black.setForeground( Color.WHITE );
+
+        red = new JButton( "RED" );
+        red.setBackground( Color.RED );
+
+        blue = new JButton( "BLUE" );
+        blue.setBackground( Color.BLUE );
+        blue.setForeground( Color.WHITE );
+
+        green = new JButton( "GREEN" );
+        green.setBackground( Color.GREEN );
+
+
+        // ACTION LISTENER FOR BUTTONS BLACK, RED, BLUE, GREEN
+        black.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                color = Color.BLACK;
+                paintArea.setPenColor( color) ;
+                colorPanel.setBackground( color );
+            }
+        });
+
+        red.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                color = Color.RED;
+                paintArea.setPenColor( color );
+                colorPanel.setBackground( color );
+            }
+        });
+
+        blue.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                color = Color.BLUE;
+                paintArea.setPenColor( color );
+                colorPanel.setBackground( color );
+            }
+        });
+
+        green.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                color = Color.GREEN;
+                paintArea.setPenColor( color );
+                colorPanel.setBackground( color );
+            }
+        });
+
 
         // SLIDER FOR PEN SIZE
-        JLabel penSizeLabel = new JLabel("Pen size :");
-        this.sliderPenSize = new JSlider(JSlider.HORIZONTAL, 0, 50, 10);
-        JLabel valueSlider = new JLabel("Size = " + this.sliderPenSize.getValue());
+        JLabel penSizeLabel = new JLabel( "Pen size :" );
+        sliderPenSize = new JSlider( JSlider.HORIZONTAL, 0, 50, 10 );
+        JLabel valueSlider = new JLabel( "Size = " + sliderPenSize.getValue() );
 
         // PEN SIZE CHANGE LISTENER
-        this.sliderPenSize.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
+        sliderPenSize.addChangeListener( new ChangeListener() {
+            public void stateChanged( ChangeEvent e ) {
                 int size = sliderPenSize.getValue();
-                paintArea.setPointRadius(size);
-                valueSlider.setText("Size = " + size);
+                paintArea.setPointRadius( size );
+                valueSlider.setText( "Size = " + size );
             }
         });
 
-        // GUM
-        this.gum = new JButton("GUM");
 
-        gum.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+        // ERASER
+        Icon eraserIcon = new ImageIcon( "./src/assets/img/eraser_icon.png" );
+        eraser = new JButton( "ERASER", eraserIcon );
+
+        eraser.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
                 color = paintArea.getBackgroundColor();
-                paintArea.setPenColor(color);
-                colorPanel.setBackground(color);
+                paintArea.setPenColor( color );
+                colorPanel.setBackground( color );
             }
         });
+
+
+        // RESET
+        Icon resetIcon = new ImageIcon( "./src/assets/img/synchronizing_icon.png" );
+        reset = new JButton( "RESET", resetIcon );
+
+        reset.addActionListener( new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                paintArea.resetArea();
+                colorPanel.setBackground( Color.BLACK );
+            }
+        });
+
+
+        // BACK
+        Icon backIcon = new ImageIcon( "./src/assets/img/arrow-back_icon.png" );
+        back = new JButton( "RETURN", backIcon );
+
+        back.addActionListener( new ActionListener() {
+           public void actionPerformed( ActionEvent e ) {
+               if ( paintArea.getPointListSize() > 0 ) {
+                   paintArea.removeLastElement();
+               }
+           }
+        });
+
 
         // PAINT AREA
         paintArea = new PaintArea();
 
-        gridPanel.add(colorLabel);
-        gridPanel.add(colorPanel);
-        gridPanel.add(colorPick);
+        // GRID PANEL ADD COMPONENTS
+        gridPanel.add( colorLabel );
+        gridPanel.add( colorPanel );
+        gridPanel.add( colorPick );
 
-        gridPanel.add(penSizeLabel);
-        gridPanel.add(this.sliderPenSize);
-        gridPanel.add(valueSlider);
+        gridPanel.add( colors );
+        gridPanel.add( red );
+        gridPanel.add( green );
+        gridPanel.add( blue );
+        gridPanel.add( black );
 
-        gridPanel.add(gum);
+        gridPanel.add( penSizeLabel );
+        gridPanel.add( sliderPenSize );
+        gridPanel.add( valueSlider );
 
-        this.colorWestPanel = new Color(204, 204, 204);
+        gridPanel.add( eraser );
+        gridPanel.add( reset );
+        gridPanel.add( back );
 
-        gridPanel.setBackground(this.colorWestPanel);
-        this.west.add(gridPanel);
-        this.add(paintArea);
-        add(this.west, BorderLayout.WEST);
+
+        // KEY LISTENER
+        addKeyListener( new KeyListener() {
+
+            // CTRL + Z EVENT
+            @Override
+            public void keyTyped( KeyEvent keyEvent ) {;}
+
+            @Override
+            public void keyPressed( KeyEvent keyEvent ) {
+                if ( keyEvent.getKeyCode() == KeyEvent.VK_Z && paintArea.getPointListSize() > 0 )
+                    paintArea.removeLastElement();
+            }
+            @Override
+            public void keyReleased( KeyEvent keyEvent ){
+                if ( keyEvent.getKeyCode() == KeyEvent.VK_Z && paintArea.getPointListSize() > 0 )
+                    paintArea.removeLastElement();
+            }
+
+        });
+        setFocusable( true );
+        setFocusTraversalKeysEnabled( false );
+
+        // ADD GRID PANEL
+        west.add( gridPanel );
+        west.setBorder( BorderFactory.createEmptyBorder( 40,0,0,0 ) );
+
+        // ADD TOOL PANEL AND PAINT AREA
+        add( paintArea );
+        add( west, BorderLayout.WEST );
     }
 
+    // MAIN
     public static void main(final String[] args) {
         new Paint();
     }
--- Paint/src/PaintArea.java
+++ Paint/src/PaintArea.java
@@ -3,9 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.event.*;
 
-public class PaintArea extends JPanel{
+public class PaintArea extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
@@ -16,82 +15,96 @@ public class PaintArea extends JPanel{
 
     public PaintArea() {
 
-        this.addMouseListener(new MouseEventHandler());
-        this.addMouseMotionListener(new MouseMotionHandler());
+        this.addMouseListener( new MouseEventHandler() );
+        this.addMouseMotionListener( new MouseMotionHandler() );
 
-        backgroundColor = new Color(255, 255, 255);
-        penColor = new Color(0, 0, 0);
-        this.setBackground(backgroundColor);
+        backgroundColor = new Color( 255, 255, 255 );
+        penColor = new Color( 0, 0, 0 );
+        this.setBackground( backgroundColor );
 
         pointList = new ArrayList<Point>();
         pointR = 10;
     }
 
-    public void paintComponent(Graphics graphics) {
-        super.paintComponent(graphics);
+    public void paintComponent( Graphics graphics ) {
+        super.paintComponent( graphics );
 
-        for (Point point : pointList) {
-            point.drawPoint(graphics);
+        for ( Point point : pointList ) {
+            point.drawPoint( graphics );
         }
     }
 
-    public void setBackgroundColor(Color color) {
+    public void setBackgroundColor( Color color ) {
         this.backgroundColor = color;
     }
 
-    public void setPenColor(Color color) {
+    public void setPenColor( Color color ) {
         this.penColor = color;
     }
 
-    public void setPointRadius(int radius) {
+    public void setPointRadius( int radius ) {
         this.pointR = radius;
     }
 
     public Color getBackgroundColor() { return this.backgroundColor; }
 
+    public int getPointListSize() { return this.pointList.size(); }
+
+    public void removeLastElement() {
+        this.pointList.remove( this.pointList.size() - 1 );
+        repaint();
+    }
+
     public void resetArea() {
-        backgroundColor = new Color(255, 255, 255);
-        penColor = new Color(0, 0, 0);
+        backgroundColor = new Color( 255, 255, 255 );
+        penColor = new Color( 0, 0, 0 );
         pointList.clear();
         repaint();
     }
 
     private class MouseEventHandler implements MouseListener {
-        public void mousePressed(MouseEvent e) {
-            pointX = e.getX() - (pointR / 2);
-            pointY = e.getY() - (pointR / 2);
+        @Override
+        public void mousePressed( MouseEvent e ) {
+            pointX = e.getX() - ( pointR / 2 );
+            pointY = e.getY() - ( pointR / 2 );
 
-            pointList.add(new Point(pointX, pointY, pointR, penColor));
+            pointList.add( new Point( pointX, pointY, pointR, penColor ) );
             repaint();
         }
 
-        public void mouseReleased(MouseEvent e) {
+        @Override
+        public void mouseReleased( MouseEvent e ) {
             ;
         }
 
-        public void mouseClicked(MouseEvent e) {
+        @Override
+        public void mouseClicked( MouseEvent e ) {
             ;
         }
 
-        public void mouseEntered(MouseEvent e) {
+        @Override
+        public void mouseEntered( MouseEvent e ) {
             ;
         }
 
-        public void mouseExited(MouseEvent e) {
+        @Override
+        public void mouseExited( MouseEvent e ) {
             ;
         }
     }
 
     private class MouseMotionHandler implements MouseMotionListener {
-        public void mouseDragged(MouseEvent e) {
-            pointX = e.getX() - (pointR / 2);
-            pointY = e.getY() - (pointR / 2);
+        @Override
+        public void mouseDragged( MouseEvent e ) {
+            pointX = e.getX() - ( pointR / 2 );
+            pointY = e.getY() - ( pointR / 2 );
 
-            pointList.add(new Point(pointX, pointY, pointR, penColor));
+            pointList.add( new Point( pointX, pointY, pointR, penColor ) );
             repaint();
         }
 
-        public void mouseMoved(MouseEvent e) {
+        @Override
+        public void mouseMoved( MouseEvent e ) {
             ;
         }
     }
